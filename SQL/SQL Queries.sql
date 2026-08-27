bf556c97d4b20d695bacbe2d4eaf31bb2e3d6ed9
Yvonne Akinyi Amugaga <amugagayvonne@gmail.com>
2026-08-27 18:30:06 +0200

    SQL queries for the dataset

--- SQL/SQL Queries.sql
+++ SQL/SQL Queries.sql
@@ -16,6 +16,15 @@ order by totalquantity desc
 limit 5;
 
 --query for low-profit products (profit margin < 10%)
-SELECT "Product Name",Category, (sum(profit)/sum(Sales)) as profitmargin FROM Superstores GROUP by "Product Name",Category HAVING profitmargin<0.1 ORDER by profitmargin DESC;
+SELECT "Product ID", "Product Name", Category, (SUM("Profit") / SUM("Sales")) AS ProfitMargin
+FROM sales
+GROUP BY "Product ID", "Product Name", Category
+HAVING ProfitMargin < 0.1
+ORDER BY ProfitMargin DESC;
 
-
+--query for sales by region
+SELECT region,
+sum(sales) as totalsales
+from sales
+group by region
+order by totalsales desc;
