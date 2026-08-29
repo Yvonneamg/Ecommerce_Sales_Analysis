b3eaeda5aacc4ad7ba19576fc10604c4de6e254c
Yvonne Akinyi Amugaga <amugagayvonne@gmail.com>
2026-08-30 00:01:55 +0200

    Updating SQL queries exploring the dataset and answering the raised business questions

--- SQL/SQL Queries.sql
+++ SQL/SQL Queries.sql
@@ -28,3 +28,61 @@ sum(sales) as totalsales
 from sales
 group by region
 order by totalsales desc;
+
+-- query for sales and profit by year
+SELECT
+    strftime('%Y', "Order Date") AS Year,
+    SUM(Sales) AS TotalSales,
+    SUM(Profit) AS TotalProfit,
+    SUM(Profit) / SUM(Sales) AS ProfitMargin
+FROM sales
+GROUP BY Year
+ORDER BY Year;
+
+-- query for sales and profit by month
+SELECT
+    strftime('%Y-%m', "Order Date") AS YearMonth,
+    SUM(Sales) AS TotalSales,
+    SUM(Profit) AS TotalProfit
+FROM sales
+GROUP BY YearMonth
+ORDER BY YearMonth;
+
+-- query for top products by total sales
+Select "Product ID",
+"Product Name",
+sum(Sales) as TotalSales
+from sales
+group by "Product ID", "Product Name"
+order by TotalSales desc;
+
+-- What product sales the most
+SELECT
+    "Product ID",
+    "Product Name",
+    SUM(Quantity) AS TotalQuantity,
+    SUM(Sales) AS TotalSales
+FROM sales
+GROUP BY "Product ID", "Product Name"
+ORDER BY TotalQuantity DESC
+LIMIT 10;
+
+-- What categorygenerates more revenue
+SELECT "Product ID",
+"Product Name",
+"Category",
+SUM(Sales) AS TotalSales,
+SUM(Profit) AS TotalProfit
+from sales
+group by "Category"
+order by TotalSales desc;
+
+-- What product generates more revenue
+SELECT "Product ID",
+"Product Name",
+SUM(Sales) AS TotalSales,
+SUM(Profit) AS TotalProfit
+from sales
+group by "Product ID", "Product Name"
+order by TotalSales desc
+limit 10;
